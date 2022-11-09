bd6bd33d09dac0f34b3af877cbd08e319a7a326f
JM <j.maxwell.alan@gmail.com>
2022-11-09 01:09:56 -0500

    add effect resistance to mobs

--- src/entities/mob/mod.rs
+++ src/entities/mob/mod.rs
@@ -2,14 +2,19 @@ mod mob_mortal;
 
 use bevy::prelude::*;
 
-use super::{damage::Damage, effect::CurrentEffect};
+use super::{
+  damage::Damage,
+  effect::{CurrentEffect, EffectResistance},
+};
 
-#[derive(Debug, Clone, Component, Default, Reflect)]
+#[derive(Debug, Default, Clone, Reflect, Component)]
 #[reflect(Component)]
 pub struct Mob {
   max_health: f32,
   current_health: f32,
   speed: f32,
   damage: Damage,
+  damage_resistance: Damage,
   current_effect: CurrentEffect,
+  effect_resistance: EffectResistance,
 }
