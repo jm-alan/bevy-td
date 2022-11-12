55800d829c128aa5068c2f3845013717b44c2dbd
JM <j.maxwell.alan@gmail.com>
2022-11-12 04:51:59 -0500

    damage from tower kind

---
+++ src/entities/damage/damage_from_tower_kind.rs
@@ -0,0 +1,52 @@
+use super::Damage;
+use crate::{entities::tower::TowerKind, traits::fusable::Fusable};
+
+impl From<TowerKind> for Damage {
+  fn from(kind: TowerKind) -> Self {
+    match kind {
+      TowerKind::Standard(components) => Self {
+        standard: components
+          .combat_stats
+          .damage,
+        ..Default::default()
+      },
+      TowerKind::Fire(components) => Self {
+        fire: components
+          .combat_stats
+          .damage,
+        ..Default::default()
+      },
+      TowerKind::Ice(components) => Self {
+        ice: components
+          .combat_stats
+          .damage,
+        ..Default::default()
+      },
+      TowerKind::Lightning(components) => Self {
+        lightning: components
+          .combat_stats
+          .damage,
+        ..Default::default()
+      },
+      TowerKind::Acid(components) => Self {
+        acid: components
+          .combat_stats
+          .damage,
+        ..Default::default()
+      },
+      TowerKind::Explosive(components) => Self {
+        explosive: components
+          .combat_stats
+          .damage,
+        ..Default::default()
+      },
+      TowerKind::Hybrid(kinds) => {
+        let damage = Damage::default();
+        for kind in kinds.into_iter() {
+          damage.fuse(kind.into());
+        }
+        damage
+      }
+    }
+  }
+}
