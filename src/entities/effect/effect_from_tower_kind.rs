3d93dc6b56114fbad41d1e60cedbcf4322a0126d
JM <j.maxwell.alan@gmail.com>
2022-11-12 05:04:36 -0500

    effect from tower kidn

---
+++ src/entities/effect/effect_from_tower_kind.rs
@@ -0,0 +1,41 @@
+use super::Effect;
+use crate::{entities::tower::TowerKind, traits::fusable::Fusable};
+
+impl From<TowerKind> for Effect {
+  fn from(kind: TowerKind) -> Self {
+    match kind {
+      TowerKind::Fire(components) => Self {
+        burn: components
+          .effect_stats
+          .into(),
+        ..Default::default()
+      },
+      TowerKind::Ice(components) => Self {
+        freeze: components
+          .effect_stats
+          .into(),
+        ..Default::default()
+      },
+      TowerKind::Lightning(components) => Self {
+        shock: components
+          .effect_stats
+          .into(),
+        ..Default::default()
+      },
+      TowerKind::Acid(components) => Self {
+        corrode: components
+          .effect_stats
+          .into(),
+        ..Default::default()
+      },
+      TowerKind::Hybrid(kinds) => {
+        let mut effect = Self::default();
+        for kind in kinds.into_iter() {
+          effect = effect.fuse(kind.into());
+        }
+        effect
+      }
+      _ => Self::default(),
+    }
+  }
+}
