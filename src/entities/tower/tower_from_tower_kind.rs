f9fb6c18976147f8bb0b676346cfd5c5ce0d2f76
JM <j.maxwell.alan@gmail.com>
2022-11-12 05:24:59 -0500

    tower from tower kind

--- src/entities/tower/tower_from_tower_kind.rs
+++ src/entities/tower/tower_from_tower_kind.rs
@@ -1,64 +1,92 @@
 use bevy::time::Timer;
 use uuid::Uuid;
 
-use super::{
-  Tower, TowerComponents,
-  TowerKind::{self, *},
-};
-use crate::entities::{
-  damage::Damage,
-  effect::{CurrentEffect, Effect},
+use super::{Tower, TowerComponents, TowerKind};
+use crate::{
+  entities::{
+    damage::Damage,
+    effect::{CurrentEffect, Effect},
+  },
+  traits::fusable::{Fusable, RecursiveFusable},
 };
 
 impl From<TowerKind> for Tower {
+  #[inline(always)]
   fn from(kind: TowerKind) -> Self {
-    let uuid = Uuid::new_v4().to_string();
-    let components: TowerComponents;
+    let uuid: String = Uuid::new_v4().to_string();
     let name: String;
-    let damage: Damage;
-    let damage_effect: Effect;
+    let mut components: TowerComponents = kind.clone().into();
+    let mut damage: Damage = kind.clone().into();
+    let mut damage_effect: Effect = kind.clone().into();
 
-    match kind {
-      Standard(comps) => {
-        name = format!("standard-tower-{uuid}").to_string();
-        components = comps;
+    match &kind {
+      TowerKind::Standard(_) => {
+        name = format!("standard-tower-{uuid}");
       }
-      Fire(comps) => {
-        name = format!("fire-tower-{uuid}").to_string();
-        components = comps;
+      TowerKind::Fire(_) => {
+        name = format!("fire-tower-{uuid}");
       }
-      Ice(comps) => {
-        name = format!("ice-tower-{uuid}").to_string();
-        components = comps;
+      TowerKind::Ice(_) => {
+        name = format!("ice-tower-{uuid}");
       }
-      Lightning(comps) => {
-        name = format!("lightning-tower-{uuid}").to_string();
-        components = comps;
+      TowerKind::Lightning(_) => {
+        name = format!("lightning-tower-{uuid}");
       }
-      Acid(comps) => {
-        name = format!("acid-tower-{uuid}").to_string();
-        components = comps;
+      TowerKind::Acid(_) => {
+        name = format!("acid-tower-{uuid}");
       }
-      Explosive(comps) => {
-        name = format!("explosive-tower-{uuid}").to_string();
-        components = comps;
+      TowerKind::Explosive(_) => {
+        name = format!("explosive-tower-{uuid}");
       }
-      Hybrid(kinds) => todo!(),
+      TowerKind::Hybrid(kinds) => {
+        name = format!("explosive-tower-{uuid}");
+        let fused_kinds = kinds
+          .clone()
+          .recursive_fuse();
+        let mut fused_damages = Damage::default();
+        let mut fused_effects = Effect::default();
+        let mut fused_components = TowerComponents::default();
+        for el in fused_kinds.into_iter() {
+          fused_damages = fused_damages.fuse(el.clone().into());
+          fused_effects = fused_effects.fuse(el.clone().into());
+          fused_components = fused_components.fuse(el.into());
+        }
+        damage = fused_damages;
+        damage_effect = fused_effects;
+        components = fused_components;
+      }
     };
 
     Self {
       name,
       damage,
       damage_effect,
-      max_health: components.max_health,
-      current_health: components.max_health,
-      range: components.range,
-      projectile_speed: components.projectile_speed,
-      kind: kind.clone(),
+      max_health: components
+        .base_stats
+        .max_health,
+      current_health: components
+        .base_stats
+        .max_health,
+      range: components
+        .combat_stats
+        .range,
+      projectile_speed: components
+        .combat_stats
+        .projectile_speed,
+      kind,
       current_effect: CurrentEffect::default(),
-      damage_timer: Timer::from_seconds(1.0 / components.fire_rate, true),
+      damage_timer: Timer::from_seconds(
+        1.0
+          / components
+            .combat_stats
+            .fire_rate,
+        true,
+      ),
       healing_timer: Timer::from_seconds(0.001, true),
-      healing_amount: components.healing_amount / 1000.0,
+      healing_amount: components
+        .base_stats
+        .healing_amount
+        / 1000.0,
     }
   }
 }
