060b91bd020e92584da595d7c69c970cce3ca04c
JM <j.maxwell.alan@gmail.com>
2022-11-12 15:20:39 -0500

    restructure tower from tower kind

--- src/entities/tower/tower_from_tower_kind.rs
+++ src/entities/tower/tower_from_tower_kind.rs
@@ -14,46 +14,54 @@ impl From<TowerKind> for Tower {
   #[inline(always)]
   fn from(kind: TowerKind) -> Self {
     let uuid: String = Uuid::new_v4().to_string();
-    let name: String;
-    let mut components: TowerComponents = kind.clone().into();
-    let mut damage: Damage = kind.clone().into();
-    let mut damage_effect: Effect = kind.clone().into();
+    let components: TowerComponents;
+    let damage: Damage;
+    let damage_effect: Effect;
 
     match &kind {
-      TowerKind::Standard(_) => {
-        name = format!("standard-tower-{uuid}");
-      }
-      TowerKind::Fire(_) => {
-        name = format!("fire-tower-{uuid}");
-      }
-      TowerKind::Ice(_) => {
-        name = format!("ice-tower-{uuid}");
-      }
-      TowerKind::Lightning(_) => {
-        name = format!("lightning-tower-{uuid}");
-      }
-      TowerKind::Acid(_) => {
-        name = format!("acid-tower-{uuid}");
-      }
-      TowerKind::Explosive(_) => {
-        name = format!("explosive-tower-{uuid}");
-      }
       TowerKind::Hybrid(kinds) => {
-        name = format!("explosive-tower-{uuid}");
-        let fused_kinds = kinds
-          .clone()
-          .recursive_fuse();
+        let fused_kinds = kinds.clone().recursive_fuse();
         let mut fused_damages = Damage::default();
         let mut fused_effects = Effect::default();
         let mut fused_components = TowerComponents::default();
-        for el in fused_kinds.into_iter() {
-          fused_damages = fused_damages.fuse(el.clone().into());
-          fused_effects = fused_effects.fuse(el.clone().into());
-          fused_components = fused_components.fuse(el.into());
+        for fused_kind in fused_kinds.into_iter() {
+          fused_damages = fused_damages.fuse(fused_kind.clone().into());
+          fused_effects = fused_effects.fuse(fused_kind.clone().into());
+          fused_components = fused_components.fuse(fused_kind.into());
         }
         damage = fused_damages;
         damage_effect = fused_effects;
         components = fused_components;
+      }
+      _ => {
+        components = kind.clone().into();
+        damage = kind.clone().into();
+        damage_effect = kind.clone().into();
+      }
+    }
+
+    let name: String = match &kind {
+      TowerKind::Standard(_) => format!("standard-tower-{uuid}"),
+      TowerKind::Fire(_) => format!("fire-tower-{uuid}"),
+      TowerKind::Ice(_) => format!("ice-tower-{uuid}"),
+      TowerKind::Lightning(_) => format!("lightning-tower-{uuid}"),
+      TowerKind::Acid(_) => format!("acid-tower-{uuid}"),
+      TowerKind::Explosive(_) => format!("explosive-tower-{uuid}"),
+      TowerKind::Hybrid(kinds) => {
+        let mut composed_name = "hybrid".to_string();
+        for kind in kinds.iter() {
+          match kind {
+            TowerKind::Standard(_) => composed_name += "-standard",
+            TowerKind::Fire(_) => composed_name += "-fire",
+            TowerKind::Ice(_) => composed_name += "-ice",
+            TowerKind::Lightning(_) => composed_name += "-lightning",
+            TowerKind::Acid(_) => composed_name += "-acid",
+            TowerKind::Explosive(_) => composed_name += "-explosive",
+            TowerKind::Hybrid(_) => panic!("Failed to recursively fuse all nested hybrids before iterator consumption"),
+          };
+        }
+        composed_name += "-tower";
+        format!("{composed_name}-{uuid}")
       }
     };
 
@@ -61,32 +69,18 @@ impl From<TowerKind> for Tower {
       name,
       damage,
       damage_effect,
-      max_health: components
-        .base_stats
-        .max_health,
-      current_health: components
-        .base_stats
-        .max_health,
-      range: components
-        .combat_stats
-        .range,
-      projectile_speed: components
-        .combat_stats
-        .projectile_speed,
+      max_health: components.base_stats.max_health,
+      current_health: components.base_stats.max_health,
+      range: components.combat_stats.range,
+      projectile_speed: components.combat_stats.projectile_speed,
       kind,
       current_effect: CurrentEffect::default(),
       damage_timer: Timer::from_seconds(
-        1.0
-          / components
-            .combat_stats
-            .fire_rate,
+        1.0 / components.combat_stats.fire_rate,
         true,
       ),
       healing_timer: Timer::from_seconds(0.001, true),
-      healing_amount: components
-        .base_stats
-        .healing_amount
-        / 1000.0,
+      healing_amount: components.base_stats.healing_amount / 1000.0,
     }
   }
 }
